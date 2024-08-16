edf0c94b1eccf87d89c9d0e91b32fac003b37971
ajaimes <armando.jaimes@ferreyros.com.pe>
2024-08-16 11:20:18 -0500

    Carousel autoplay plugin

--- app/dashboard/carousel/page.tsx
+++ app/dashboard/carousel/page.tsx
@@ -10,16 +10,17 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 
-import Autoplay from 'embla-carousel-autoplay'
+
 
 
 export default function Page() {
   return (
     <div className="w-full flex justify-center">
       <Carousel className="w-full max-w-xs"
-        plugins={[Autoplay({
-          delay: 1000
-        })]}
+        autoplay= {1500}
+        // plugins={[Autoplay({
+        //   delay: 1000
+        // })]}
         opts={{
           // dragFree: true,
           loop: true,
