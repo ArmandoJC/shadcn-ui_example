1420923dbce7dd7166993a65c82cb3a3ef2243d7
ajaimes <armando.jaimes@ferreyros.com.pe>
2024-09-05 11:33:48 -0500

    Formato de celdas

--- app/dashboard/data-table/columns.tsx
+++ app/dashboard/data-table/columns.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Badge } from "@/components/ui/badge"
 import { Payment } from "@/data/payments.data"
 import { ColumnDef } from "@tanstack/react-table"
 
@@ -8,8 +9,8 @@ import { ColumnDef } from "@tanstack/react-table"
 
 export const columns: ColumnDef<Payment>[] = [
     {
-        accessorKey: "status",
-        header: "Status",
+        accessorKey: "clientName",
+        header: "Client Name",
     },
     {
         accessorKey: "email",
@@ -17,10 +18,30 @@ export const columns: ColumnDef<Payment>[] = [
     },
     {
         accessorKey: "amount",
-        header: "Amount",
+        header: () => <div className="text-right">Amount</div>,
+        cell: ({ row }) => {
+            const amount = parseFloat(row.getValue("amount"))
+            const formatted = new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+            }).format(amount)
+
+            return <div className="text-right font-medium">{formatted}</div>
+        },
     },
     {
-        accessorKey: "clientName",
-        header: "Client Name",
+        accessorKey: "status",
+        header: "Status",
+        cell: ({ row }) => {
+            const status = row.getValue("status") as string;
+            const variant = {
+                pending: 'secondary',
+                processing: 'info',
+                success: 'success',
+                failed: 'destructive',
+            }[status] ?? ('default') as any;
+
+            return <Badge variant={variant} capitalize>{status}</Badge>
+        },
     },
 ]
